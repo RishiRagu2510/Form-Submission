cc1504e6d45a85e9b1eeb6bad0f54ac29ea57b0b
RishiRagu2510 <rishiragu25@gmail.com>
2023-10-09 12:32:22 +0530

    Form updated

---
+++ formsubmission/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleForm from './Components/SimpleForm';
+
+function App() {
+  return (
+  <SimpleForm/>
+  );
+}
+
+export default App;

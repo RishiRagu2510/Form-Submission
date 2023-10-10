e9daebf7ce9e93de3c9e2e0285e60a55400a21fd
RishiRagu2510 <rishiragu25@gmail.com>
2023-10-10 11:11:58 +0530

    form updated

--- formsubmission/src/Components/SimpleForm.js
+++ formsubmission/src/Components/SimpleForm.js
@@ -6,17 +6,17 @@ const SimpleForm = () => {
     <div>
     
     <form action={console.log(input)}>
-    <label>Name:</label>
-    <input type='text'></input>
-    <label>RegNo:</label>
-    <input type='text'></input>
-    <button onClick={(e)=>setInput(e.target.value)}>submit</button>
+    <label id='username'>Enter the UserName
+    <input type='text'></input></label>
+    <br></br>
+    <label id='password'>Enter the Password
+    <input type='password'></input></label>
     </form>
-    
-    
-    
+    <br></br>
+    <button onClick={(e)=>setInput(alert("form Submitted"))}>Submit</button>
+
     </div>
   )
 }
 
-export default SimpleForm
+export default SimpleForm;
